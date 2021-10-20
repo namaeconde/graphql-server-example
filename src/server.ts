8475021ec3fa8dde74e1a9ea65625b237868d2cd
Namae Conde <lunamae.conde@sensis.com.au>
2021-10-21 01:01:15 +1100

    Set server to connect to mongodb before listening to port

--- src/server.ts
+++ src/server.ts
@@ -6,26 +6,37 @@ import cors from "cors";
 import { makeExecutableSchema } from "graphql-tools";
 import typedefs from "./typedefs";
 import resolvers from "./resolvers";
+import models, { connectDb } from "./db";
 
 export const schema = makeExecutableSchema({
   typeDefs: typedefs,
   resolvers: resolvers,
 });
 
-(async function startApolloServer() {
-  const PORT = process.env.PORT || 4001;
-  const app = express();
-  app.use(cors());
-  app.use(compression());
+const PORT = process.env.PORT || 4001;
+const app = express();
+app.use(cors());
+app.use(compression());
 
+(async function startApolloServer() {
   const server = new ApolloServer({
-      schema
+    schema,
+    context: async ({ req, res }) => {
+      return {
+        req,
+        res,
+        models
+      }
+    }
   });
   await server.start();
 
-  server.applyMiddleware({ app, path: "/graphql" });
-  const httpServer = createServer(app);
-  httpServer.listen({ port: PORT }, (): void =>
-    console.log(`🚀GraphQL-Server is running on http://localhost:${PORT}/graphql`)
-  );
+  connectDb().then(async (result) => {
+    console.log(`Connected to ${result.connection.db.namespace}`);
+    server.applyMiddleware({ app, path: "/graphql" });
+    const httpServer = createServer(app);
+    httpServer.listen({ port: PORT }, (): void =>
+      console.log(`🚀GraphQL-Server is running on http://localhost:${PORT}/graphql`)
+    );
+  })
 })();
